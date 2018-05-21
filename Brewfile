5b1388c58fe3e931aab4df6ead75cb2028861e36
Justin Eveland <jbeveland27@gmail.com>
2018-05-20 22:15:31 -0400

    Initial Commit

---
+++ Brewfile
@@ -0,0 +1,130 @@
+cask_args appdir: '/Applications'
+
+# Taps
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+tap 'caskroom/versions'
+tap 'heroku/brew'
+tap 'homebrew/bundle'
+tap "homebrew/core"
+
+
+# Install ZSH
+# Make sure /usr/bin/env/zsh is appended to /etc/shells (this is done in install.sh)
+brew 'zsh'
+brew 'zsh-completions'
+
+# Install GNU core utilities (those that come with macOS are outdated)
+brew 'coreutils'
+
+# Install some other useful utilities like `sponge`.
+brew  'moreutils'
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew 'findutils'
+
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew 'gnu-sed', args ['with-default-names']
+
+# Install Bash 4
+brew 'bash'
+brew 'bash-completion2'
+
+# Install more recent versions of some macOS tools
+brew 'grep'
+
+# Install Useful Binaries
+brew 'awscli'
+brew 'ack'
+brew 'dockutil'
+brew 'git' #look at atlassian setup guide to remove outdated version of git
+brew 'git-lfs'
+brew 'heroku/brew/heroku'
+brew 'httpie'
+brew 'imagemagick'
+brew 'mackup'
+brew 'mas'
+brew 'openssl'
+brew 'rlwrap'
+brew 'tree'
+brew 'unar'
+brew 'wget'
+
+# Development
+brew 'clojure'
+brew 'mongodb'
+brew 'postgresql'
+brew 'rbenv'
+brew 'sqlite'
+brew 'the_silver_searcher'
+brew 'tmux'
+brew 'vim'
+brew 'watchman'
+brew "yarn", args: ["without-node"]
+
+# GUI Apps
+cask '1password'
+cask 'atom'
+cask 'db-browser-for-sqlite'
+cask 'dropbox'
+cask 'firefoxdeveloperedition'
+cask 'google-chrome'
+cask 'github'
+cask 'handbrake'
+cask 'imageoptim'
+cask 'iterm2'
+cask 'java'
+cask 'mamp'
+cask 'parallels'
+cask 'sequel-pro'
+cask 'slack'
+cask 'sourcetree'
+cask 'spectacle'
+cask 'spotify'
+cask 'sql-operations-studio'
+cask 'sublime-text'
+cask 'vagrant'
+cask 'virtualbox'
+cask 'virtualbox-extension-pack'
+cask 'visual-studio-code'
+cask 'visual-studio-code-insiders'
+cask 'zoomus'
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+cask 'qlcolorcode'
+cask 'qlmarkdown'
+cask 'quicklook-json'
+cask 'quicklook-csv'
+cask 'qlstephen'
+cask 'qlprettypatch'
+cask 'betterzipql'
+cask 'qlimagesize'
+cask 'webpquicklook'
+cask 'suspicious-package'
+
+# Fonts
+cask 'font-source-code-pro-for-powerline'
+cask 'font-source-code-pro'
+cask 'font-source-sans-pro'
+cask 'font-source-serif-pro'
+
+# Font tools
+tap 'bramstein/webfonttools'
+brew 'sfnt2woff'
+brew 'sfnt2woff-zopfli'
+brew 'woff2'
+
+# # Install Mac App Store apps
+# mas '1Password', id: 443987910
+# mas 'Byword', id: 420212497
+# mas 'Deckset', id: 847496013
+# mas 'Gapplin', id: 768053424
+# mas 'Giphy Capture', id: 668208984
+# mas 'Paw', id: 584653203
+# mas 'Sip', id: 507257563
+# mas 'Spark', id: 1176895641
+# mas 'The Unarchiver', id: 425424353
+# mas 'Things', id: 904280696
+# mas 'Tweetbot', id: 1384080005
+# mas 'Unsplash Wallpapers', id: 1284863847
+# mas 'WhatsApp', id: 1147396723 
---
+++ install.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+# This borrows on ideas present in thoughtbot/laptop/mac and driesvints/dotfiles/install.sh
+
+###############################################################################
+# Functions                                                                   #
+###############################################################################
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+append_to_zshrc() {
+  local text="$1" zshrc
+  local skip_new_line="${2:-0}"
+
+  if [ -w "$HOME/.zshrc.local" ]; then
+    zshrc="$HOME/.zshrc.local"
+  else
+    zshrc="$HOME/.zshrc"
+  fi
+
+  if ! grep -Fqs "$text" "$zshrc"; then
+    if [ "$skip_new_line" -eq 1 ]; then
+      printf "%s\n" "$text" >> "$zshrc"
+    else
+      printf "\n%s\n" "$text" >> "$zshrc"
+    fi
+  fi
+}
+
+update_shell() {
+  local shell_path;
+  shell_path="$(which zsh)"
+
+  fancy_echo "Changing your shell to zsh ..."
+  if ! grep "$shell_path" /etc/shells > /dev/null 2>&1 ; then
+    fancy_echo "Adding '$shell_path' to /etc/shells"
+    sudo sh -c "echo $shell_path >> /etc/shells"
+  fi
+  sudo chsh -s "$shell_path" "$USER"
+}
+
+###############################################################################
+# Run Setup                                                                   #
+###############################################################################
+
+fancy_echo "Setting up your Mac..."
+
+# The Brewfile handles Homebrew-based app and library installs, but there may
+# still be updates and installables in the Mac App Store.
+fancy_echo "› sudo softwareupdate -i -a"
+sudo softwareupdate -i -a
+
+# Make sure xcode command line utils are installed...this step could take awhile
+xcode-select --install
+
+# Check for Homebrew and install if we don't have it
+if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
+  fancy_echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
+  return
+elif test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update --force # https://github.com/Homebrew/brew/issues/1151
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install all our dependencies with bundle (See Brewfile)
+brew bundle
+
+######################################################################
+# Make ZSH the default shell environment (ZSH is installed in Brewfile)
+case "$SHELL" in
+  */zsh)
+    if [ "$(which zsh)" != '/usr/local/bin/zsh' ] ; then
+      update_shell
+    fi
+    ;;
+  *)
+    update_shell
+    ;;
+esac
+chsh -s $(which zsh)
+
+# Install PHP extensions with PECL
+#pecl install imagick
+
+# Install Composer
+#curl -sS https://getcomposer.org/installer | php
+#mv composer.phar /usr/local/bin/composer
+
+# Install global Composer packages
+#/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet
+
+# Install Laravel Valet
+#$HOME/.composer/vendor/bin/valet install
+
+# Install NVM
+if [ ! -f "$HOME/.nvm/nvm.sh" ]
+then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+fi
+source ~/.nvm/nvm.sh
+nvm install --lts
+
+
+# Create Development directories
+mkdir $HOME/Development
+mkdir $HOME/Development/GitHub
+mkdir $HOME/Development/Graphics
+mkdir $HOME/Development/VSCodeWorkspaces
+
+# Symlink the Mackup config file to the home directory
+#ln -s ./.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source macos/.macos
