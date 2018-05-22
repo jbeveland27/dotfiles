0788e8e1685a98ece725631e16e9c531efdb0712
Justin Eveland <jbeveland27@gmail.com>
2018-05-22 13:49:26 -0400

    Fixing some issues with dock defaults

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,23 +2,22 @@
 # Replace dock apps with preferred default list
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Finder.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/FirefoxDeveloperEdition.app"
+dockutil --no-restart --add "/Applications/Firefox Developer Edition.app"
 
-dockutil --no-restart --add "/Applications/VerticalBar.app"
+dockutil --no-restart --add "/Dropbox/Apps/VerticalBar.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Notes.app"
 
-dockutil --no-restart --add "/Applications/VerticalBar.app"
+dockutil --no-restart --add "/Dropbox/Apps/VerticalBar 2.app"
 dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Utilities/iTunes.app"
+dockutil --no-restart --add "/Applications/iTunes.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 
-dockutil --no-restart --add "/Applications/VerticalBar.app"
+dockutil --no-restart --add "/Dropbox/Apps/VerticalBar 3.app"
 dockutil --no-restart --add "/Applications/1Password 6.app"
 dockutil --no-restart --add "/Applications/Sourcetree.app"
 dockutil --no-restart --add "/Applications/Sequel Pro.app"
@@ -26,12 +25,12 @@ dockutil --no-restart --add "/Applications/MAMP/MAMP.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code - Insiders.app"
 dockutil --no-restart --add "/Applications/Sublime Text.app"
 dockutil --no-restart --add "/Applications/SQL Operations Studio.app"
-dockutil --no-restart --add "/Applications/Terminal.app"
+dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/CodeKit.app"
 dockutil --no-restart --add "/Applications/DB Browser for SQLite.app"
 dockutil --no-restart --add "/Applications/Cyberduck.app"
 
-dockutil --no-restart --add "/Applications/VerticalBar.app"
+dockutil --no-restart --add "/Dropbox/Apps/VerticalBar 4.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 
